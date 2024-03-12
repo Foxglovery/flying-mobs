d31452f17219ce91b315f9c9c882ff989d25a6cb
Foxglovery <foxglovery@gmail.com>
2024-03-12 12:52:10 -0500

    for sprite 2

--- script.js
+++ script.js
@@ -10,10 +10,12 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = './assets/enemy1.png';
-    // this.speed = Math.random() * 4 - 2;
-    this.spriteWidth = 293;
-    this.spriteHeight = 155;
+    this.image.src = "./assets/enemy2.png";
+    //sprite 1
+    // this.spriteWidth = 293;
+    // this.spriteHeight = 155;
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
     //divide by three to keep the ratio good
     this.width = this.spriteWidth / 3;
     this.height = this.spriteHeight / 2.5;
@@ -21,10 +23,14 @@ class Enemy {
     this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 6 + 3);
+    this.speed = Math.random() * 4 + 1;
   }
   update() {
-    this.x += Math.random() * 5 - 2.5;
-    this.y += Math.random() * 5 - 2.5;
+    //sprite 1
+    // this.x += Math.random() * 5 - 2.5;
+    this.x -= this.speed;
+    if (this.x + this.width < 0) this.x = canvas.width;
+    // this.y += Math.random() * 5 - 2.5;
     //animate sprite sheet
     if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
